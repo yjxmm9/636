8dbe480dc65fa8e0f9b20a2e9f2abd61fe2105e9
yjxmm <qingliuyang0618@163.com>
2022-06-23 12:42:59 +0800

    移动

---
+++ Cannel of Jinghang/Assets/Scripts/TestBoatController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBoatController : MonoBehaviour
+{
+    public float speed;
+    public float turnspeed;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(horizontal * speed * Time.deltaTime, 0, speed * Time.deltaTime);
+        Rotating(horizontal);
+        
+    }
+
+    private void Rotating(float hor)
+    {
+        //获取方向
+        Vector3 dir = new Vector3(hor, 0, 1);
+        //将方向转换为四元数
+        Quaternion quaDir = Quaternion.LookRotation(dir, Vector3.up);
+        //缓慢转动到目标点
+        transform.rotation = Quaternion.Lerp(transform.rotation, quaDir, Time.fixedDeltaTime * turnspeed);
+    }
+
+    
+}
