d77b7ebcc394e5c28948b422ba9924827884134c
yjxmm <qingliuyang0618@163.com>
2022-07-10 17:14:39 +0800

    人物模型

--- Cannel of Jinghang/Assets/Scripts/PlayerController.cs
+++ Cannel of Jinghang/Assets/Scripts/PlayerController.cs
@@ -14,13 +14,17 @@ public class PlayerController : MonoBehaviour
     //public float jumpTimer;
     public bool isjumping = false;
     public int gravity = -35;
+    private float Height;
 
-    Vector3 collStandSize, collStandOffset, collCrouchSize, collCrouchOffset;
-    BoxCollider coll;
+    Vector3  collStandOffset,  collCrouchOffset;
+    float collStandSize, collCrouchSize,collStandRadius,collCrouchRadius;
+    CapsuleCollider coll;
     private bool isCrouch = false;
 
     private bool isShield;//护盾
     public GameObject testBoat;//传入testBoat物体
+
+    private Animator pa;
 
 
 
@@ -28,13 +32,16 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        pa = this.GetComponent<Animator>();
         jump = new Vector3(0.0f, 1f, 0.0f);
 
-        coll = GetComponent<BoxCollider>();
-        collStandSize = coll.size;    //获取一开始的碰撞体大小
+        coll = GetComponent<CapsuleCollider>();
+        collStandSize = coll.height;    //获取一开始的碰撞体大小
         collStandOffset = coll.center;    //获取一开始的碰撞体位置
-        collCrouchSize = new Vector3(coll.size.x, coll.size.y / 2, coll.size.z);  //获取下蹲时候的碰撞体大小
-        collCrouchOffset = new Vector3(coll.center.x, -0.25f, coll.center.z);    //获取下蹲时候的碰撞体位置
+        collStandRadius = coll.radius;
+        collCrouchSize = coll.height/1.3f;  //获取下蹲时候的碰撞体大小
+        collCrouchOffset = new Vector3(coll.center.x, coll.center.y/1.3f, coll.center.z);    //获取下蹲时候的碰撞体位置
+        collCrouchRadius = coll.radius / 2;
         
     }
 
@@ -42,20 +49,34 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //jumpTimer += Time.deltaTime;
-        if (this.transform.position.y < 1)
+        if (CheckGrounded())
+        {//When Junkochan is on the ground
+            pa.SetBool("Grounded", true);//Set Junkochan's "Grounded" parameter in Animator component
+            pa.SetBool("Jump", false);
+        }
+        else
         {
-            isjumping = false;
+            pa.SetBool("Grounded", false);
         }
 
         Physics.gravity = new Vector3(0, gravity, 0);  // gravity= -35 其他的默认
 
-        if (Input.GetKeyDown(KeyCode.Space)&&!isjumping&&!isCrouch)
+        if (Input.GetKeyDown(KeyCode.Space)&&CheckGrounded())
         {
-            GetComponent<Animator>().SetBool("isFalling", true);
+            pa.SetBool("Jump", true);
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
             //jumpTimer = 0;
-            isjumping = true;
+            
         }
+        if (!CheckGrounded() && transform.position.y - Height < 0)
+        {//When Junkochan is falling, tansit animation state from "Ascending" to "Falling"
+            pa.SetBool("Fall", true);//Set Junkochan's "Run" parameter in Animator component
+        }
+        else
+        {
+            pa.SetBool("Fall", false);
+        }
+        Height = transform.position.y;
 
         isShield = testBoat.GetComponent<TestBoatController>().isShield;
 
@@ -65,10 +86,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Player"))
-        {
-            GetComponent<Animator>().SetBool("isFalling", false);
-        }
+        
 
         if (other.tag.Equals("Barrier"))
         {
@@ -109,20 +127,21 @@ public class PlayerController : MonoBehaviour
 
     private void Crouch()
     {
-        if (Input.GetKeyDown(KeyCode.C)&&!isjumping)
+        if (Input.GetKeyDown(KeyCode.C)&&CheckGrounded())
         {
-            isCrouch = true;
-            coll.size = collCrouchSize;
+            pa.SetBool("Crouch", true);
+            coll.height = collCrouchSize;
             coll.center = collCrouchOffset;
-            GetComponent<Animator>().SetBool("isCrouching", true);
+            //coll.radius = collCrouchRadius;
+            
             //transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y/2, transform.localScale.z);
         }
-        else if(Input.GetKeyUp(KeyCode.C) && !isjumping)
+        else if(Input.GetKeyUp(KeyCode.C) && CheckGrounded())
         {
-            isCrouch = false;
+            pa.SetBool("Crouch", false);
             coll.center = collStandOffset;
-            coll.size = collStandSize;
-            GetComponent<Animator>().SetBool("isCrouching", false);
+            coll.height = collStandSize;
+            //coll.radius = collStandRadius;
             //transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y*2, transform.localScale.z);
 
         }
@@ -131,6 +150,12 @@ public class PlayerController : MonoBehaviour
     private void Score()
     {
         UIManager.Instance.UpdateUI(1);
+    }
+
+    bool CheckGrounded()
+    {//Judge whether Junkochan is on the ground or not
+        Ray ray = new Ray(this.transform.position + Vector3.up * 0.05f, Vector3.down * 0.15f);//Shoot ray at 0.05f upper from Junkochan's feet position to the ground with its length of 0.1f
+        return Physics.Raycast(ray, 0.15f);//If the ray hit the ground, return true
     }
 
 }
