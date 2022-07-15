2d580aaf735c8d19a21dd800209748d76a51565d
zyn123123123 <1724072381@qq.com>
2022-07-15 10:03:20 +0800

    Revert "收集品"
    This reverts commit 1a7a1f74d1e3a4e414dcc203708b6760149431b2.

--- Cannel of Jinghang/Assets/Scripts/Collect.cs
+++ Cannel of Jinghang/Assets/Scripts/Collect.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class Collect : MonoBehaviour
 {
 
-
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "TestBoat" || other.tag == "Player")
         {
-            WinUI.collectionCount += 1;
-            Destroy(this.gameObject);//在收集到之后摧毁该物体
+            Destroy(this.gameObject);
+
         }
     }
 
